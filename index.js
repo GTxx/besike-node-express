b24e5a0b8c27a0be37b9eb894bfb1a374ba1ca26
GTxx <xiongxiong1986@gmail.com>
2015-01-10 23:08:12 +0800

    implement error handling

--- index.js
+++ index.js
@@ -2,22 +2,30 @@ var http = require('http');
 
 function myexpress(){
 
-
-
   function app(req, res){
-
     function createNext(){
-      var stack_idx = 0;
-      function next(){
-        if (stack[stack_idx]) {
-          stack_idx += 1;
-          return stack[stack_idx-1](req, res, next);
-        } else {
-          res.end("404 - Not Found")
+      var process_stack_idx = 0;
+      var error_stack_idx = 0;
+
+      function next() {
+        console.log(arguments)
+        if (arguments.length == 1) {
+          // error process
+          error_stack_idx += 1;
+          return error_stack[error_stack_idx - 1](arguments[0], req, res, next);
+        }
+        if (arguments.length == 0) {
+          process_stack_idx += 1;
+          try {
+            return process_stack[process_stack_idx - 1](req, res, next);
+          } catch (e) {
+            next(e);
+          }
         }
       }
       return next;
     }
+
     var next = createNext();
     next();
     //res.writeHead(404)
@@ -29,14 +37,28 @@ function myexpress(){
     return server.listen.apply(server, arguments);
   }
 
-  function error_handler(err, res, req, next){
+  function not_found_handler(req, res, next){
+    res.end("404 - Not Found")
+  }
+
+  function default_error_handler(err, req, res, next){
+    // return 500 by default
     res.end('500 - Internal Error');
   }
-  var stack = []
-  app.stack = stack;
+  var error_stack = [default_error_handler]
+  var process_stack = [not_found_handler]
+
+  //app.stack = stack;
 
   app.use = function(func){
-    stack.push(func)
+    if (func.length == 3){
+      process_stack[process_stack.length-1] = func
+      process_stack.push(not_found_handler)
+    }
+    if (func.length == 4){
+      error_stack[error_stack.length-1] = func
+      error_stack.push(default_error_handler)
+    }
   }
 
   return app;
