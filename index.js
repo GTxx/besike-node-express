c93c2bbd0b02530bcd0b4eeeb8da27a5d04e2ab1
GTxx <xiongxiong1986@gmail.com>
2015-01-17 00:19:38 +0800

    implement fancy request path matcher

--- index.js
+++ index.js
@@ -5,73 +5,52 @@ var http = require('http')
 function myexpress(){
 
   function app(req, res) {
+    //app.handle(req, res);
+    // support app(req, res, next)
     app.handle(req, res);
   }
 
   app.handle = function(req, res, out_next) {
     var stack_idx = 0;
-    var stack = this.stack;
-    req.params = {};
+    var stack = app.stack;
+
     function next(err) {
       stack_idx++;
       var hasError = Boolean(err);
       var layer = stack[stack_idx-1]
 
       if (!layer){
-        console.log('no layer left')
         if(!out_next){
-          console.log('not subapp')
           finalHandler(err, req, res)
           return
         } else {
           // handle is a subapp,
+          req.url = req.trim_url + req.url;
           out_next(err);
           return
         }
-      } else {
+      }
         var handle = layer.handle;
-        console.log(handle)
-        console.log('req.url is '+req.url)
         var url_match = layer.match(req.url);
-        console.log('url_mathch is '+url_match)
         if (url_match) {
-          if ('function' == typeof handle.handle) {
-            console.log('req.url: ' + req.url)
-            console.log('layer.url: '+layer.url)
-            req.url = req.url.split(layer.url)[1];
-            console.log('after strip, req.url: '+req.url)
-          }
           req.params = url_match.params;
-          console.log('req.params is '+req.params)
+
           try {
             if (hasError && isErrorHandle(handle)) {
               // hasError and handle is function(err, req, res, next)
-              console.log('handle error')
               handle(err, req, res, next);
-              if ('function' == typeof layer.handle.handle) {
-                req.url = layer.url + req.url;
-                console.log('restore req.url: '+req.url)
-              }
-              return
+
             } else if (!hasError && !isErrorHandle(handle)) {
               // has no error and handle is function(req, res, next)
-              console.log('handle normal')
-              console.log('--------------')
               handle(req, res, next);
-              if ('function' == typeof layer.handle.handle) {
-                req.url = layer.url + req.url;
-                console.log('restore req.url: '+req.url)
-              }
-              return
+
             }
 
           } catch (e) {
             err = e;
           }
-
         }
         next(err)
-      }
     }
     next();
   }
@@ -91,10 +70,12 @@ function myexpress(){
       url = '/'
     }
     if ('function' == typeof func.handle){
-      //var subapp = func;
-      //func = function(req, res, next){
-      //  subapp.handle(req, res, next)
-      //}
+      var subapp = func;
+      func = function(req, res, next){
+        req.url = req.url.split(url)[1];
+        req.trim_url = url;
+        subapp.handle(req, res, next)
+      }
     }
     var layer = new Layer(url, func)
     this.stack.push(layer)
